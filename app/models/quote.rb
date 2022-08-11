bbad5a8d26a2c38a05517dffe5b3d88ce00c2b84
Lukas Musilek <lukas.musilek@gmail.com>
2022-08-11 17:15:22 +0100

    prepared navbar to show total sum of all line items + styling

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,10 +2,15 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
   belongs_to :company
 
   scope :ordered, -> { order(id: :desc) }
 
   # after_create_commit -> { broadcast_prepend_to "quotes" }
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
